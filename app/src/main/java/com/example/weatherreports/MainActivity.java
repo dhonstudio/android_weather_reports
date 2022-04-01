cc2a161fcee4d9cbeb29d4a11124972a4653904f
dhonstudio <doon13@gmail.com>
2022-04-01 15:15:34 +0700

    Create Main

--- app/src/main/java/com/example/weatherreports/MainActivity.java
+++ app/src/main/java/com/example/weatherreports/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.weatherreports;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReports;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign value
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReports = findViewById(R.id.lv_weatherReports);
+
+        //click listener
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 2.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 2.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You type " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
